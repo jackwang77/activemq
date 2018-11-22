96d1ceeb64e7a84d84ee7afcddb4ed5cfaadd71b
jackwang77 <31368260+jackwang77@users.noreply.github.com>
2018-11-22 13:59:59 +0800

    创建了一个学生类

---
+++ activemq-provider/src/main/java/bhz/entity/Student.java
@@ -0,0 +1,58 @@
+package bhz.entity;
+
+/**
+ * @author wangjun
+ * @date 2018/11/22
+ */
+public class Student {
+
+    private String username;
+
+    private String password;
+
+    private Integer sex;
+
+    private String phone;
+
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
