677248bb460589c0222c9aeff3b2b7cb15389cde
jackwang77 <31368260+jackwang77@users.noreply.github.com>
2018-11-22 14:35:47 +0800

    测试看是否能提交到另一个分支

--- activemq-consumer/src/main/java/bhz/entity/School.java
+++ activemq-consumer/src/main/java/bhz/entity/School.java
@@ -12,6 +12,17 @@ public class School {
 
    private String musicName;
 
+   private String code;
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
     public Integer getId() {
         return Id;
     }
